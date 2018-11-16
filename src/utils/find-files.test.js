e545db93d6a1a7a18616baa709c547e6393164c4
Giancarlo Anemone <ganemone@uber.com>
2018-11-16 15:49:29 -0500

    Update js file APIs to use globs and respect .gitignores

--- src/utils/find-files.test.js
+++ src/utils/find-files.test.js
@@ -25,7 +25,7 @@ THE SOFTWARE.
 import {findFiles} from './find-files.js';
 
 test('findFiles', async () => {
-  const files = await findFiles('src', f => {
+  const files = await findFiles('src/**/*', f => {
     return /find-files.test.js/.test(f);
   });
   expect(files.length).toEqual(1);
