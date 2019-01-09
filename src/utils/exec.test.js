7b302ed9e623b3106765a6c5ce0fd525ac679751
Chris Deacy <chrisdothtml@gmail.com>
2019-01-08 16:48:26 -0800

    Add docs and test for exec options
    https://github.com/DubstepJS/core/pull/54

--- src/utils/exec.test.js
+++ src/utils/exec.test.js
@@ -27,3 +27,7 @@ import {exec} from './exec.js';
 test('exec', async () => {
   await expect(exec('echo test')).resolves.toEqual('test');
 });
+
+test('exec with options', async () => {
+  await expect(exec('pwd', {cwd: __dirname})).resolves.toEqual(__dirname);
+});
